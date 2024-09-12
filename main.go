1b1f1eba32bf2f7e526fc01e8aa6c2da3e5c19a7
AmrTamer23 <amrtamer23mj@gmail.com>
2024-09-12 22:20:51 +0300

    feat: implemented a search for file func and ignored node_modules

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 var CSSFilesToLookFor = [...]string{
 	"main.css",
 	"app.css",
@@ -8,5 +14,47 @@ var CSSFilesToLookFor = [...]string{
 	"global.css",
 }
 
+func searchFile(root, fileName string) (string, error) {
+
+	var foundPath string
+
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() && d.Name() == "node_modules" {
+			return filepath.SkipDir
+		}
+
+		if !d.IsDir() && d.Name() == fileName {
+			foundPath = path
+			return filepath.SkipDir
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return foundPath, nil
+}
+
 func main() {
+	fmt.Println("Searching for CSS file")
+
+	for i := 0; i < len(CSSFilesToLookFor); i++ {
+
+		fmt.Println("Searching for ", CSSFilesToLookFor[i])
+
+		var search, _ = searchFile(".", CSSFilesToLookFor[i])
+
+		fmt.Println(search)
+
+		if search != "" {
+			fmt.Println("Found")
+		}
+	}
 }
